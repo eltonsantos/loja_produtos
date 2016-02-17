9012abf622d6da96d484bee2e43cc11020d213f9
Elton Santos <elton@r2a.net.br>
2016-02-17 16:40:49 -0300

    Comentando algumas coisas

--- app/controllers/conta/pedidos_controller.rb
+++ app/controllers/conta/pedidos_controller.rb
@@ -1,6 +1,7 @@
 class Conta::PedidosController < ApplicationController
   before_action :authenticate_usuario!
 
+  # Mostra os pedidos da pessoa que está logada
   def index
     @pedidos = current_usuario.pessoa.pedidos.order("id DESC")
   end
